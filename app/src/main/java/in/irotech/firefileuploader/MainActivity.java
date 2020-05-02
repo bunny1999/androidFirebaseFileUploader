04a6efe177505a235920118588c59a611143f2a0
IroTech <souravrawat1999@gmail.com>
2020-05-02 23:26:41 +0530

    firebase file uploder

---
+++ app/src/main/java/in/irotech/firefileuploader/MainActivity.java
@@ -0,0 +1,73 @@
+package in.irotech.firefileuploader;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button button;
+    StorageReference firebaseStorage;
+    ProgressDialog progressDialog;
+    final static int REQUESTCODE=4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.imageView);
+        button=findViewById(R.id.button);
+
+        firebaseStorage=FirebaseStorage.getInstance().getReference();
+        progressDialog=new ProgressDialog(MainActivity.this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,REQUESTCODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==REQUESTCODE){
+            progressDialog.setMessage("Uploading...");
+            Uri uri=data.getData();
+            progressDialog.show();
+            imageView.setImageURI(uri);
+
+            StorageReference fileName=firebaseStorage.child("Photo/image"+uri.getLastPathSegment()+".png");
+            fileName.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(MainActivity.this,"Uploaded",Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this,"Uploaded",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+    }
+}
